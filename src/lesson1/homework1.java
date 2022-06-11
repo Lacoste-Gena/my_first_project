cd196a18407dec6016f54ca2105d09c66683594e
Lacoste-Gena <galiyevg@yandex.ru>
2022-06-12 01:01:42 +0500

    домашка

---
+++ src/lesson1/homework1.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+public class homework1 {
+    public static void main(String[] args) {
+        //Базовый уровень
+        //Задача №1
+        //Дано (их менять нельзя)
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        //Создать из трех переменных единую строку,
+        //Привести к правильному виду (Ниже), убрать лишние слова,
+        //затроить (Можно вызвать тольку одну команду System.out.print())
+        //
+        //Результат вывода на экран:
+        //Hello world!
+        //Hello world!
+        //Hello world!
+        String myString = hi.trim() + world.toLowerCase();
+        System.out.print((myString + newLine).repeat(3));
+    }
+
+}
--- src/test2.java
+++ src/test2.java
@@ -2,5 +2,6 @@ public class test2 {
     public static void main(String[] args) {
         System.out.print("my name is Gena");
 
+
     }
 }
