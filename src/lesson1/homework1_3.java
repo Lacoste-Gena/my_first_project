f3fd4eb9f40a4c46e4e9a0d11dff04b00ca50201
Lacoste-Gena <galiyevg@yandex.ru>
2022-06-12 03:08:31 +0500

    домашка1_3

--- src/lesson1/homework1_3.java
+++ src/lesson1/homework1_3.java
@@ -1,5 +1,22 @@
 package lesson1;
 
 public class homework1_3 {
+//Задача №3
+    //Создать из массива букв a,b,c,d,e, строку,вывести на экран , поменять в массиве 4 букву по счету на h,
+    //и снова создать строку, вывести на экран
 
+    public static void main(String[] args) {
+
+        char[] myArray = new char[5];
+        myArray[0] = 'a';
+        myArray[1] = 'b';
+        myArray[2] = 'c';
+        myArray[3] = 'd';
+        myArray[4] = 'e';
+
+
+        char[] myArray2 = myArray;
+        myArray[3] = 'h';
+        System.out.print(myArray2);
+    }
 }
